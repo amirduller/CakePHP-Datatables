6769d2c7168efd0c65ba545f9db7dc6399ea71aa
amirduller <cnmd_29@yahoo.com>
2012-01-27 21:37:07 +0800

    Update example app/app/models/part.php

--- example app/app/models/part.php
+++ example app/app/models/part.php
@@ -2,4 +2,14 @@
 class Part extends AppModel {
 	var $name = 'Part';
 	var $displayField = 'name';
+	
+	var $belongsTo = array(
+		'Tag' => array(
+			'className' => 'Tag',
+			'foreignKey' => 'tag_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		),
+	);
 }
